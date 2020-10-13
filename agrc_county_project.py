bc893f0d0ef8f64e098cb239087dfce6efe874f4
Jake Adams <jdadams@utah.gov>
2020-10-13 14:27:57 -0600

    fix: Paths and variable names

--- agrc_county_project.py
+++ agrc_county_project.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import arcpy
 import os
 import csv
@@ -104,41 +106,41 @@ def createCsvRow(row):
 
 if __name__ == '__main__':
 
-    uniqueRunNum = strftime("%Y%m%d_%H%M%S")
-    print(uniqueRunNum)
+    unique_run_id = strftime("%Y%m%d_%H%M%S")
+    print(unique_run_id)
 
     county_ids = ['49055']
 
-    countyProjectTemplate = r'c:\gis\git\usps-county-project\County_Project_Submission_Template.gdb\CP_Submit_template'
+    schema_template = r'c:\gis\git\usps-county-project\County_Project_Submission_Template.gdb\CP_Submit_template'
     sgid_connection = r'c:\gis\projects\fastdata\internal.agrc.utah.gov.sde'
 
     #: Source data
     addressPointsWorkspace = r'c:\gis\projects\fastdata\USPSAddress\Address.gdb'
-    addressPoints = 'WayneCo20200923'
-    addressPoints = os.path.join(addressPointsWorkspace, addressPoints)
+    source_fc_name = 'WayneCo20200923'
+    source_fc_path = Path(addressPointsWorkspace, source_fc_name)
 
     #: Set up output locations
     print('Setting up output spaces...')
-    output_folder = r'c:\gis\projects\fastdata\USPSAddress\out2'
-    outputGdb_name = 'outputs.gdb'
-    outputGdb = os.path.join(output_folder, outputGdb_name)
+    output_folder = Path(r'c:\gis\projects\fastdata\USPSAddress\out2')
+    output_gdb_name = 'outputs.gdb'
+    output_gdb_path = Path(output_folder, output_gdb_name)
 
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
-    if not arcpy.Exists(outputGdb):
-        arcpy.CreateFileGDB_management(output_folder, outputGdb_name)
+    output_folder.mkdir(parents=True, exist_ok=True)
+
+    if not arcpy.Exists(str(output_gdb_path)):
+        arcpy.CreateFileGDB_management(output_folder, output_gdb_name)
 
     #: Pre-CSV output feature class
-    countyProject = os.path.join(outputGdb, 'countyProject_{}'.format(uniqueRunNum))
+    output_fc_path = Path(output_gdb_path, f'countyProject_{unique_run_id}')
     arcpy.CreateFeatureclass_management(
-        outputGdb, 'countyProject_{}'.format(uniqueRunNum), template=countyProjectTemplate
+        str(output_gdb_path), f'countyProject_{unique_run_id}', template=schema_template
     )
 
     #: District identity datasets
-    congressionalDistricts = os.path.join(sgid_connection, 'SGID.POLITICAL.USCongressDistricts2012')
-    addrPointsWithDistrict = os.path.join(output_folder, outputGdb_name, 'addressPointsProject' + uniqueRunNum)
+    congressional_districts_fc_path = Path(sgid_connection, 'SGID.POLITICAL.USCongressDistricts2012')
+    identify_result_fc_path = Path(output_folder, output_gdb_name, 'addressPointsProject' + unique_run_id)
 
-    countyProjectFields = [
+    output_fields = [
         'NAME',
         'COMPANYNAME',
         'ADDRESSLINE',
@@ -188,77 +190,78 @@ if __name__ == '__main__':
 
     # Fields to be converted to USPS county project format.
     # 'CountyID', 'FullAdd', 'City', 'ZipCode' must exist in the addressPoints table.
-    addrFields = ['OID@', 'CountyID', 'FullAdd', 'City', 'ZipCode', 'DISTRICT', 'SHAPE@Y', 'SHAPE@X']
+    source_fields = ['OID@', 'CountyID', 'FullAdd', 'City', 'ZipCode', 'DISTRICT', 'SHAPE@Y', 'SHAPE@X']
 
     # Set with county_ids list
     county_selection_where = None
     if len(county_ids) > 0:
-        county_selection_where = f"CountyID IN ('{','.join(county_ids)}')"
-    address_layer = 'addrpoints_' + uniqueRunNum
-    arcpy.MakeFeatureLayer_management(addressPoints, address_layer, county_selection_where)
+        county_ids_string = ','.join(county_ids)
+        county_selection_where = f"CountyID IN ('{county_ids_string}')"
+    address_layer = 'addrpoints_' + unique_run_id
+    arcpy.MakeFeatureLayer_management(str(source_fc_path), address_layer, county_selection_where)
 
     #: Get the congressional district for each address point
     print('Running Congressional District identify...')
     arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(4326)
     arcpy.env.geographicTransformations = 'NAD_1983_To_WGS_1984_5'
-    arcpy.Identity_analysis(address_layer, congressionalDistricts, addrPointsWithDistrict)
+    arcpy.Identity_analysis(address_layer, str(congressional_districts_fc_path), str(identify_result_fc_path))
 
     #: Copy address points w/district info to countyProject feature class
     print('Copying data to output feature class and list...')
-    with arcpy.da.SearchCursor(addrPointsWithDistrict, addrFields) as addrPointCursor,\
-         arcpy.da.InsertCursor(countyProject, countyProjectFields) as countyProjectCursor:
+    with arcpy.da.SearchCursor(str(identify_result_fc_path), source_fields) as source_cursor,\
+         arcpy.da.InsertCursor(str(output_fc_path), output_fields) as output_cursor:
 
-        for addrRow in addrPointCursor:
-            object_id, county_id, full_address, city, zip_code, district, shape_y, shape_x = addrRow
-            countyRow = []
+        for source_address_row in source_cursor:
+            object_id, county_id, full_address, city, zip_code, district, shape_y, shape_x = source_address_row
+            output_address_row = []
             # County Project NAME
-            countyRow.append(countyFipsDomain[int(county_id)])
+            output_address_row.append(countyFipsDomain[int(county_id)])
             # County Project COMPANYNAME
-            countyRow.append('AGRC')
+            output_address_row.append('AGRC')
             # County Project ADDRESSLINE
-            countyRow.append(full_address)
+            output_address_row.append(full_address)
             # County Project CITY
-            countyRow.append(city)
+            output_address_row.append(city)
             # County Project STATE
-            countyRow.append('UT')
+            output_address_row.append('UT')
             # County Project ZIP5
-            countyRow.append(zip_code)
+            output_address_row.append(zip_code)
             # County Project ZIP4
-            countyRow.append(0)
+            output_address_row.append(0)
             # County Project CONGRESSIONALCODE
-            countyRow.append(f'UT0{district}')
+            output_address_row.append(f'UT0{district}')
             # County Project COUNTYCODE
-            countyRow.append(f'UT{county_id[-3:]}')
+            output_address_row.append(f'UT{county_id[-3:]}')
             # County Project FILLER
-            countyRow.append('')
+            output_address_row.append('')
             # County Project KEY
-            countyRow.append(object_id)
+            output_address_row.append(object_id)
             # County Project LATITUDE
-            countyRow.append(shape_y)
+            output_address_row.append(shape_y)
             # County Project LONGITUDE
-            countyRow.append(shape_x)
+            output_address_row.append(shape_x)
             # CountyProject row insert
-            countyProjectCursor.insertRow(countyRow)
+            output_cursor.insertRow(output_address_row)
             # County project rows
-            counties[countyRow[0]].append(countyRow)
+            counties[output_address_row[0]].append(output_address_row)
 
-    headerString = ','.join(countyProjectFields)
+    headerString = ','.join(output_fields)
     # List will be created for each id in county_ids
     # Output file should be a csv
     print('Writing csv...')
     for county in counties:
-        countyProjectCsv = os.path.join(output_folder, '{}_{}.csv'.format(county, uniqueRunNum))
+        output_csv_path = Path(output_folder, f'{county}_{unique_run_id}.csv')
         row_list = counties[county]
         if len(row_list) > 0:
-            with open(countyProjectCsv, 'w') as outputFile:
-                outputFile.write(headerString + '\n')
+            with open(output_csv_path, 'w') as output_file:
+                output_file.write(headerString + '\n')
                 for row in row_list:
                     try:
                         rowString = createCsvRow([str(x) for x in row])
-                        outputFile.write(rowString + '\n')
+                        output_file.write(rowString + '\n')
                     except:
                         print(row)
 
-                outputFile.write('\n')
+                output_file.write('\n')
 
     print('! - complete - !')
